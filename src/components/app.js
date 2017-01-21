46a2be6454108c13349338ce1da2458ed6a699aa
Brice Tessier Huort <tessierhuort@gmail.com>
2017-01-21 17:05:07 +0100

    implement blueprint

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import {
+    Button,
+    Menu,
+    MenuItem,
+    MenuDivider,
+    Popover,
+    Position
+} from "@blueprintjs/core";
+
+const menu = (
+    <Menu>
+        <MenuItem text="New" />
+        <MenuItem text="Open" />
+        <MenuItem text="Save" />
+        <MenuDivider />
+        <MenuItem text="Settings..." />
+    </Menu>
+);
+
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        React simple starter
+        <Popover content={menu} position={Position.BOTTOM_RIGHT}>
+          <Button text="Actions" />
+        </Popover>
+      </div>
     );
   }
 }
